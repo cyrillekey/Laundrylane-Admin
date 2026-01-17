e3aad67c51c9a14ef529e6ea55d741239cf8cf6f
cyrille <cyrilleotieno7@gmail.com>
2026-01-17 21:39:21 +0300

    fix update password

--- container/forms/opt-form.tsx
+++ container/forms/opt-form.tsx
@@ -1,24 +1,39 @@
-import { GalleryVerticalEnd } from "lucide-react"
+"use client";
+import { GalleryVerticalEnd } from "lucide-react";
 
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 import {
   Field,
   FieldDescription,
   FieldGroup,
   FieldLabel,
-} from "@/components/ui/field"
+  FieldError,
+} from "@/components/ui/field";
 import {
   InputOTP,
   InputOTPGroup,
   InputOTPSeparator,
   InputOTPSlot,
-} from "@/components/ui/input-otp"
+} from "@/components/ui/input-otp";
+import { useForm } from "@tanstack/react-form";
+import z from "zod";
+import { Spinner } from "@/components/ui/spinner";
 
 export function OTPForm({ className, ...props }: React.ComponentProps<"div">) {
+  const form = useForm({
+    defaultValues: { otp: "" },
+    validators: { onSubmit: z.object({ otp: z.string().min(6) }) },
+  });
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <form>
+      <form
+        noValidate
+        onSubmit={(e) => {
+          e.preventDefault();
+          form.handleSubmit();
+        }}
+      >
         <FieldGroup>
           <div className="flex flex-col items-center gap-2 text-center">
             <a
@@ -35,35 +50,54 @@ export function OTPForm({ className, ...props }: React.ComponentProps<"div">) {
               We sent a 6-digit code to your email address
             </FieldDescription>
           </div>
-          <Field>
-            <FieldLabel htmlFor="otp" className="sr-only">
-              Verification code
-            </FieldLabel>
-            <InputOTP
-              maxLength={6}
-              id="otp"
-              required
-              containerClassName="gap-4"
-            >
-              <InputOTPGroup className="gap-2.5 *:data-[slot=input-otp-slot]:h-16 *:data-[slot=input-otp-slot]:w-12 *:data-[slot=input-otp-slot]:rounded-md *:data-[slot=input-otp-slot]:border *:data-[slot=input-otp-slot]:text-xl">
-                <InputOTPSlot index={0} />
-                <InputOTPSlot index={1} />
-                <InputOTPSlot index={2} />
-              </InputOTPGroup>
-              <InputOTPSeparator />
-              <InputOTPGroup className="gap-2.5 *:data-[slot=input-otp-slot]:h-16 *:data-[slot=input-otp-slot]:w-12 *:data-[slot=input-otp-slot]:rounded-md *:data-[slot=input-otp-slot]:border *:data-[slot=input-otp-slot]:text-xl">
-                <InputOTPSlot index={3} />
-                <InputOTPSlot index={4} />
-                <InputOTPSlot index={5} />
-              </InputOTPGroup>
-            </InputOTP>
-            <FieldDescription className="text-center">
-              Didn&apos;t receive the code? <a href="#">Resend</a>
-            </FieldDescription>
-          </Field>
-          <Field>
-            <Button type="submit">Verify</Button>
-          </Field>
+          <form.Field name="otp">
+            {(field) => {
+              const isInvalid =
+                field.state.meta.isTouched && !field.state.meta.isValid;
+              return (
+                <Field aria-invalid={isInvalid}>
+                  <FieldLabel htmlFor="otp" className="sr-only">
+                    Verification code
+                  </FieldLabel>
+                  <InputOTP
+                    maxLength={6}
+                    name={field.name}
+                    aria-invalid={isInvalid}
+                    id={field.name}
+                    onChange={field.handleChange}
+                    required
+                    containerClassName="gap-4"
+                  >
+                    <InputOTPGroup className="gap-2.5 *:data-[slot=input-otp-slot]:h-16 *:data-[slot=input-otp-slot]:w-12 *:data-[slot=input-otp-slot]:rounded-md *:data-[slot=input-otp-slot]:border *:data-[slot=input-otp-slot]:text-xl">
+                      <InputOTPSlot index={0} aria-invalid={isInvalid} />
+                      <InputOTPSlot index={1} aria-invalid={isInvalid} />
+                      <InputOTPSlot index={2} aria-invalid={isInvalid} />
+                    </InputOTPGroup>
+                    <InputOTPSeparator aria-invalid={isInvalid} />
+                    <InputOTPGroup className="gap-2.5 *:data-[slot=input-otp-slot]:h-16 *:data-[slot=input-otp-slot]:w-12 *:data-[slot=input-otp-slot]:rounded-md *:data-[slot=input-otp-slot]:border *:data-[slot=input-otp-slot]:text-xl">
+                      <InputOTPSlot index={3} aria-invalid={isInvalid} />
+                      <InputOTPSlot index={4} aria-invalid={isInvalid} />
+                      <InputOTPSlot index={5} aria-invalid={isInvalid} />
+                    </InputOTPGroup>
+                  </InputOTP>
+                  <FieldError errors={field.state.meta.errors} />
+                  <FieldDescription className="text-center">
+                    Didn&apos;t receive the code? <a href="#">Resend</a>
+                  </FieldDescription>
+                </Field>
+              );
+            }}
+          </form.Field>
+          <form.Subscribe>
+            {({ isSubmitting }) => (
+              <Field>
+                <Button type="submit">
+                  {isSubmitting && <Spinner />}
+                  Verify
+                </Button>
+              </Field>
+            )}
+          </form.Subscribe>
         </FieldGroup>
       </form>
       <FieldDescription className="px-6 text-center">
@@ -71,5 +105,5 @@ export function OTPForm({ className, ...props }: React.ComponentProps<"div">) {
         and <a href="#">Privacy Policy</a>.
       </FieldDescription>
     </div>
-  )
+  );
 }
